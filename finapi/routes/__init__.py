06cd227bdec774df468dec4a23edcc51311c08a0
Decloquement Oscar <odecloquement@gmail.com>
2023-11-12 04:38:21 +0100

    Update __init__

---
+++ finapi/routes/__init__.py
@@ -0,0 +1,4 @@
+__author__ = "Fyleek"
+__description__ = "Defines API routes and endpoints"
+__copyright__ = "Copyright 2023, Fyleek"
+__licence__ = "MIT"
---
+++ finapi/schemas/__init__.py
@@ -0,0 +1,8 @@
+from .Login import Login
+
+__all__ = ["Login"]
+
+__author__ = "Fyleek"
+__description__ = "Contains data validation schemas and serialization/deserialization logic"
+__copyright__ = "Copyright 2023, Fyleek"
+__licence__ = "MIT"
