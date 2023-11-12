a2fa9b9a65b8f1127192e028f692e36542586084
Decloquement Oscar <odecloquement@gmail.com>
2023-11-12 04:40:34 +0100

    Add constants

---
+++ finapi/constants.py
@@ -0,0 +1,6 @@
+API_ROOT = "https://api.finary.com"
+COOKIE_FILE = "cookie.txt"
+DEVICE_ID = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+
+# For local purpose only
+CREDENTIALS_JSON = "credentials.json"
