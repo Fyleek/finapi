77d69f856ab2afd65a04d4e015ffb68fc7fc522d
Decloquement Oscar <odecloquement@gmail.com>
2023-11-15 19:40:00 +0100

    Use get_request instead of duplicating lines

--- finapi/services/users_service.py
+++ finapi/services/users_service.py
@@ -1,17 +1,7 @@
 from finapi.constants import API_ROOT
 from finapi.models.responses import FinaryResponse
-from finapi.utils import get_session
+from finapi.utils import get_session, get_request
 
 
 def get_organizations_request() -> FinaryResponse:
-    organizations_url = f"{API_ROOT}/users/me/organizations"
-    session = get_session()
-    response = session.get(organizations_url)
-    status_code = response.status_code
-    response_json = response.json()
-    return FinaryResponse(
-        response_json.get("result"),
-        response_json.get("message"),
-        response_json.get("error"),
-        status_code
-    ).response()
+    return get_request("users/me/organizations")
