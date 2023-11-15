d9dfd8fcc5d817f1de4c3541b88b8af67c4797e0
Decloquement Oscar <odecloquement@gmail.com>
2023-11-15 19:40:42 +0100

    Remove unused variables

--- finapi/services/users_service.py
+++ finapi/services/users_service.py
@@ -1,6 +1,5 @@
-from finapi.constants import API_ROOT
 from finapi.models.responses import FinaryResponse
-from finapi.utils import get_session, get_request
+from finapi.utils import get_request
 
 
 def get_organizations_request() -> FinaryResponse:
