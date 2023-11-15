5eb9d1587cdcb965725edb986ea0c8eb62a00a37
Decloquement Oscar <odecloquement@gmail.com>
2023-11-15 19:35:12 +0100

    Update get_session function

--- finapi/utils/session_handler.py
+++ finapi/utils/session_handler.py
@@ -1,8 +1,12 @@
 import requests
 import http.cookiejar
 
+from finapi.constants import COOKIE_FILE
 
-def get_session(cookies: http.cookiejar.MozillaCookieJar) -> requests.Session:
+
+def get_session() -> requests.Session:
     session = requests.Session()
+    cookies = http.cookiejar.MozillaCookieJar(COOKIE_FILE)
+    cookies.load()
     session.cookies = cookies
     return session
