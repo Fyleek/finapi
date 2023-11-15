79d1f5dd367b48ccdbe8bf734ac38898c90271c5
Decloquement Oscar <odecloquement@gmail.com>
2023-11-15 16:32:09 +0100

    Update with new Responses Models

--- finapi/services/auth_service.py
+++ finapi/services/auth_service.py
@@ -3,13 +3,13 @@ import json
 import logging
 
 from finapi.constants import API_ROOT, COOKIE_FILE
-from finapi.models import AuthServiceResponse
+from finapi.models import FinapiResponse
 from finapi.utils.credentials_handler import get_credentials, get_credentials_mfa
 from finapi.utils.header_handler import get_header_login
 from finapi.utils.session_handler import get_session
 
 
-def sign_in_request(mfa_code: str) -> AuthServiceResponse:
+def sign_in_request(mfa_code: str):
     signin_url = f"{API_ROOT}/auth/signin"
 
     credentials = get_credentials()
@@ -24,7 +24,7 @@ def sign_in_request(mfa_code: str) -> AuthServiceResponse:
     if response.status_code == 201:  # NO MFA
         cookies.save()
         logging.info("Sign-in NO MFA OK")
-        return AuthServiceResponse.success(message="Sign-in done")
+        return FinapiResponse.success(message="Sign-in done")
     elif response.status_code == 202:  # MFA
         otp_relay_token = response.json()["result"]["otp_relay_token"]
         mfa_data = get_credentials_mfa(mfa_code, otp_relay_token)
@@ -33,14 +33,12 @@ def sign_in_request(mfa_code: str) -> AuthServiceResponse:
         if response.status_code == 201:
             cookies.save()
             logging.info("Sign-in MFA OK")
-            return AuthServiceResponse.success(message="Sign-in done")
+            return FinapiResponse.success(message="Sign-in done")
         else:
             logging.error(f"Sign-in fail MFA KO | ERROR-CODE:{response.status_code}")
-            return AuthServiceResponse.error(
-                status_code=response.status_code,
-                detail=response.reason,
-                message=json.loads(response.text)["error"]["code"],
+            return FinapiResponse.error(
+                status_code=response.status_code, message=json.loads(response.text)["error"]["code"]
             )
     else:
         logging.error(f"Sign-in fail | ERROR-CODE: {response.status_code}")
-        return AuthServiceResponse.error(status_code=response.status_code, detail=response.reason)
+        return FinapiResponse.error(status_code=response.status_code, message=response.reason)
